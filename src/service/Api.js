6bdd6d0eae3fa86fbeb8fc55099cf36e3fded68a
Rosie <bbhyeone@gmail.com>
2021-08-06 23:29:07 +0900

    Test redux-saga
    Setting redux-saga with reduxjs-toolkit.

---
+++ src/service/Api.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+async function fetchData() {
+    const url = 'https://api.punkapi.com/v2/beers';
+    const { data } = await axios.get(url);
+    return data;
+}
+
+export { fetchData };
+
+
